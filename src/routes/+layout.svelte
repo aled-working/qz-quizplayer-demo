7825aa99751ad13a37124e6c8d3ffa47645937e5
aled-working <aledsignup@gmail.com>
2023-03-26 12:41:22 +0100

    welcome page and pearson base css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script>
+    import logo from '$lib/assets/logo.png';
+  </script>
+
+
+<main class='center'>
+<nav>nav</nav>
+<slot />
+
+</main>
+<footer class='center'>
+    <div>12 questions created for KS3 learners by</div>
+    <div><img src={logo} alt=""></div>
+    
+</footer>
+
+
+<style>
+    main{
+        inset:0;
+        min-height: 90vh; 
+        background: radial-gradient(64.57% 64.57% at 50% 35.43%, #009BC7 0%, #007A9C 92.65%);
+        
+        color:white;
+        position: relative;
+    }
+    nav{
+        position: absolute;
+        top:0;
+        padding: 1rem;
+    }
+    footer{
+        color: var(--pearson-blue);
+        min-height: max-content; 
+        padding: 4rem;
+        gap: 2rem
+    }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Microscopes and magnification <span class='faint'>quiz</span></h1>
+<h1 class="faint">Ready?</h1>
+<button class="primary btn go-arrow"><h1>Start &#9656;</h1> </button>
+
+
+<style>
+
+button.primary {
+    border: none;
+    background: white;
+    padding: 0 2rem;
+    border-radius: 4rem;
+    color: var(--pearson-blue);
+    MARGIN: 2rem 0
+}  
+
+</style>
